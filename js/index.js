978b8821f1ce62f9881998ad4f2102b009db998e
Matheus Compagnoni <compagnoni_santos@hotmail.com>
2024-09-05 20:25:54 -0300

    adicionando os spans no html e a sua devida classe do css

---
+++ js/index.js
@@ -0,0 +1,26 @@
+// const input = document.querySelectorAll(".input");
+// const botaoEnviar = document.getElementById('idBotao')
+
+// botaoEnviar.addEventListener('click,' function(event){
+//     event.preventDefault(); 
+
+//     console.log(botaoEnviar)
+
+//     validarCampos()
+// })
+
+
+
+// TENTATIVA DE MUDAR AS CORES DA CAIXA DO FORMULARIO
+// input.forEach(input => {
+//     input.addEventListener('change', function() {
+//         if(input.value !== "") {
+//             input.classList.add("campo-preenchido")
+//         } else{
+//             input.classList.remove("campo-preenchido")
+//         }
+
+//     })
+// })
+
+
