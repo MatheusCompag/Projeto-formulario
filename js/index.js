f745f993ae98cfbddfec6babb044e08620ee135f
Matheus Compagnoni <compagnoni_santos@hotmail.com>
2024-09-06 20:45:15 -0300

    adicionando a função do javscript onde a mensagem de campo obrigatio apareça caso o input esteja vazio

--- js/index.js
+++ js/index.js
@@ -3,22 +3,25 @@ const botaoEnviar = document.querySelector('.enviar');
 const spans = document.querySelectorAll(".erro-msg")
 
 
-botaoEnviar.addEventListener('click', (event) =>{
-    event.preventDefault()
+botaoEnviar.addEventListener('click', (event) => {
+    event.preventDefault();
 
 
-    inputs.forEach(input => {
+    inputs.forEach((input, index) => {
+        const span = spans[index];
+
         if(input.value !== ""){
             input.classList.add("campo-preenchido");
             input.classList.remove("campo-vazio");
+            span.style.display = 'none';
         
         } else {
             input.classList.add("campo-vazio");
             input.classList.remove("campo-preenchido");
+            span.style.display = 'inline';
         }
     });
 
-    
 
     
 });
